666443a158d3e3c10a70ea75a35bd3193e788e36
Erin Jo Richey <richey.erin@gmail.com>
2015-07-21 22:27:43 -0700

    More changes for B3

--- templates/default/applytojoin/login.tpl.php
+++ templates/default/applytojoin/login.tpl.php
@@ -1,5 +1,5 @@
 <div class="row">
-    <div class="col-md-6 col-md-offset-6 well text-center">
+    <div class="col-md-6 col-md-offset-3 text-center">
         <p style="padding: 0; margin: 0">
             <a href="<?=\Idno\Core\site()->config()->getDisplayURL()?>account/join">Don't have an account? Click here to join.</a>
         </p>
