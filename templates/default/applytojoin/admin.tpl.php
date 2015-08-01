31afe9b28e20c9bde4e4febc5e12719331cf03d3
Ben Werdmuller <ben@benwerd.com>
2015-08-01 09:45:39 -0700

    Moving to Bootstrap 3

--- templates/default/applytojoin/admin.tpl.php
+++ templates/default/applytojoin/admin.tpl.php
@@ -1,12 +1,12 @@
 <div class="row">
 
-    <div class="span10 offset1">
+    <div class="col-md-10 col-md-offset-1">
         <?= $this->draw('admin/menu') ?>
         <h1>
             Membership Applications
         </h1>
     </div>
-    <div class="span10 offset1">
+    <div class="col-md-10 col-md-offset-1">
 
         <p class="explanation">
             <?php
@@ -31,7 +31,7 @@
 
 ?>
     <div class="row">
-        <div class="pane span10 offset1">
+        <div class="pane col-md-10 col-md-offset-1">
     <?php
 
             foreach($vars['applications'] as $user) {
@@ -41,25 +41,25 @@
                     ?>
 
                     <div class="row <?= strtolower(str_replace('\\', '-', get_class($user))); ?>">
-                        <div class="span3">
+                        <div class="col-md-3">
                             <p>
                                 <?= htmlentities($user->getTitle()) ?></a> (<a href="<?= $user->getDisplayURL() ?>"><?=$user->handle?></a>)<br>
                                 <small><?= $user->email ?></small>
                             </p>
                         </div>
-                        <div class="span2">
+                        <div class="col-md-2">
                             <p>
                                 <small><strong>Applied</strong><br><time datetime="<?= date('r', $user->created) ?>" class="dt-published"><?= date('r', $user->created) ?></time></small>
                             </p>
                         </div>
-                        <div class="span1">
+                        <div class="col-md-1">
                             <p style="padding-top: 20px;"><small>
                                     <?php
                                         echo \Idno\Core\site()->actions()->createLink(\Idno\Core\site()->config()->getDisplayURL() . 'admin/applytojoin/', '<i class="icon-plus"></i> Approve', array('user' => $user->getUUID(), 'action' => 'approve'), array('class' => '', 'confirm' => true, 'confirm-text' => 'Are you sure? This will approve this user and allow them to access the site.'));
                                     ?>
                                 </small></p>
                         </div>
-                        <div class="span1">
+                        <div class="col-md-1">
                             <p style="padding-top: 20px;"><small>
                                     <?php
                                         echo \Idno\Core\site()->actions()->createLink(\Idno\Core\site()->config()->getDisplayURL() . 'admin/applytojoin/', '<i class="icon-minus"></i> Delete', array('user' => $user->getUUID(), 'action' => 'delete'), array('class' => '', 'confirm' => true, 'confirm-text' => 'Are you sure? This will remove this application. The user will not be emailed.'));
--- templates/default/applytojoin/login.tpl.php
+++ templates/default/applytojoin/login.tpl.php
@@ -1,5 +1,5 @@
 <div class="row">
-    <div class="span6 offset3 well text-center">
+    <div class="col-md-6 col-md-offset-3 well text-center">
         <p style="padding: 0; margin: 0">
             <a href="<?=\Idno\Core\site()->config()->getDisplayURL()?>account/join">Don't have an account? Click here to join.</a>
         </p>
